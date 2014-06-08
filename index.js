2a6f238df99f9ded667ee9edd7f11678b7565975
Alexander Rysenko <rysenko@gmail.com>
2014-06-09 00:30:28 +0400

    Browserify on start

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
+var fs = require('fs');
+var bundleStream = fs.createWriteStream('./public/js/bundle.js');
+
+var browserify = require('browserify');
+var b = browserify();
+b.add('./public/js/index.js');
+b.bundle().pipe(bundleStream);
+
 app.set('port', (process.env.PORT || 5000));
 app.use('/', express.static(__dirname + '/public'));
 
